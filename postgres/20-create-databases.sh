e1954d84e33a9d0ec6d36f6fc3655433db41bd9e
Irina Alferova <irinaalferova13@mail.ru>
2023-11-06 12:51:02 +0300

    a

--- postgres/20-create-databases.sh
+++ postgres/20-create-databases.sh
@@ -4,4 +4,4 @@ set -e
 export VARIANT="v1"
 export SCRIPT_PATH=/docker-entrypoint-initdb.d/
 export PGPASSWORD=postgres
-psql -f "$SCRIPT_PATH/scripts/db-$VARIANT.sql"
+psql -f "$SCRIPT_PATH/scripts/db-v1.sql"
